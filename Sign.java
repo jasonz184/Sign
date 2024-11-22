ca5bd1426e472b15ab6b69f644758851868284a6
jasonz184 <jasonz184@nycstudents.net>
2024-11-22 16:21:07 +0000

    Added all test cases

--- Sign.java
+++ Sign.java
@@ -7,8 +7,25 @@ public class Sign
         message = m;
         width = w;
     }
+
     public int numberOfLines()
     {
-        return message.length() / width + 1;
+        int lines = message.length() / width;
+        if(message.length() % width == 0) return lines;
+        return lines + 1;
+    }
+
+    public String getLines()
+    {
+        if(message.length() == 0) return null;
+        String s = "";
+        int i = 0;
+        while(i < message.length())
+        {
+            s += message.substring(i, i + 1);
+            i++;
+            if(i % width == 0 && i != message.length()) s += ";";
+        }
+        return s;
     }
 }
